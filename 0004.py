ac81cb6d1a8a89b82a5ebb8638b805ba080af72f
Wanseok kim <wanseok.k@gmail.com>
2021-04-27 08:12:55 +0900

    4회차

---
+++ 0004.py
@@ -0,0 +1,19 @@
+s = 'show how to index into sequences'.split()
+
+print(s)
+v = list(s)
+print(v)
+u = [s]
+print(u)
+
+
+a = [1]
+print(type(a))
+a[2]
+b=(2)
+print(type(b))
+c = 1, 2, 3, 4
+print(c)
+print(list(c))
+ls = ['a', 365, True]
+print(list(ls))
